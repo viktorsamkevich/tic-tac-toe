043df1130e1d301ea086355e0ad3e553e133593d
Samkevich <D:\The Bat\E-mail>
2017-09-22 15:36:55 +0300

    all metods done

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,121 @@
 class TicTacToe {
-    constructor() {
+	constructor() {
+		this._turn = 0;
+		this._field = [
+			[null, null, null], 
+			[null, null, null], 
+			[null, null, null]
+		];
+	}
 
-    }
+	getCurrentPlayerSymbol() {
+		if (this._turn % 2 == 0) {
+			return 'x';
+		} else {
+			return 'o';
+		}
+	}
 
-    getCurrentPlayerSymbol() {
+	nextTurn(rowIndex, columnIndex) {
+		if (this._field[rowIndex][columnIndex] != null) {
+			return this._field[rowIndex][columnIndex];
+		}
 
-    }
+		this._field[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+		this._turn++;
+	}
 
-    nextTurn(rowIndex, columnIndex) {
+	_get_match_status() {
+		// Check cols
+		for(let j = 0; j < 3; j++) {
+			for(let i = 0; i < 3; i++) {
+				if(this._field[i][j] != 'x')
+					break;
+				if(i == 2)
+					return 'x';
+			}
+		}
 
-    }
+		for(let j = 0; j < 3; j++) {
+			for(let i = 0; i < 3; i++) {
+				if(this._field[i][j] != 'o')
+					break;
+				if(i == 2)
+					return 'o';
+			}
+		}
+		
+		// Check rows
+		for(let j = 0; j < 3; j++) {
+			for(let i = 0; i < 3; i++) {
+				if(this._field[j][i] != 'x')
+					break;
+				if(i == 2)
+					return 'x';
+			}
+		}
 
-    isFinished() {
+		for(let j = 0; j < 3; j++) {
+			for(let i = 0; i < 3; i++) {
+				if(this._field[j][i] != 'o')
+					break;
+				if(i == 2)
+					return 'o';
+			}
+		}
 
-    }
+		// Check diagonal
+		if (this._field[1][1] != null) {
+			if (this._field[0][0] == this._field[1][1] & this._field[2][2] == this._field[1][1]) {
+				return this._field[1][1];
+			}
+			if (this._field[2][0] == this._field[1][1] & this._field[0][2] == this._field[1][1]) {
+				return this._field[1][1];
+			}
+		}
 
-    getWinner() {
+		if (this._turn == 9) {
+			return 'draw';
+		}
 
-    }
+		return 'in_process';
+	}
 
-    noMoreTurns() {
+	isFinished() {
+		if (this._get_match_status() == 'in_process') {
+			return false;
+		}
+		return true;
+	}
 
-    }
+	getWinner() {
+		let result = this._get_match_status();
+		if (result == 'in_process' | result == 'draw') {
+			return null;
+		} else {
+			return result;
+		}
+	}
 
-    isDraw() {
+	noMoreTurns() {
+		if (this._turn == 9) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    }
+	isDraw() {
+		if (this._get_match_status() == 'draw') {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+	getFieldValue(rowIndex, colIndex) {
+		return this._field[rowIndex][colIndex];
+	}
 }
 
 module.exports = TicTacToe;
